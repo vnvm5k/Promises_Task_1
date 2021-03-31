7712fdbc2e3ee96b039a1c5cb9792113c2816171
Vladimir Vorobtsov <vnvm5k@yandex.ru>
2021-03-31 23:11:48 +0300

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
 import GameSavingLoader from './GameSavingLoader';
+import GameSaving from './GameSaving';
 
-GameSavingLoader.load()
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+GameSavingLoader.load().then((data) => {
+  const {
+    id,
+    created,
+    userInfo: {
+      id: userId,
+      name: userName,
+      level: userLevel,
+      points: userPoints,
+    },
+  } = JSON.parse(data);
+  const gameSaving = new GameSaving(id, created, {
+    userId, userName, userLevel, userPoints,
+  });
+  return gameSaving;
+});
